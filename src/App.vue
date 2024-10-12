ce91b34cc3d73d1f18327a490c730d996802e10b
Selwyn Tayong <selwyntayong12@gmail.com>
2024-10-12 17:55:05 +0800

    Add Navbar and Carousel as Reuseable component

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <AppBar />
+    <v-main>
+      <Carousel />
+      <v-container class="white-background" fluid>
+        <v-row align="center" justify="center" class="fill-height">
+          <v-col cols="12" md="8">
+            <v-btn color="primary">Welcome to My App</v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import AppBar from "./components/AppBar.vue";
+</script>
+
+<style scoped>
+.white-background {
+  background-color: white;
+  min-height: 100vh; /* Ensures it takes full screen height */
+}
+.fill-height {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ src/components/Carousel.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-carousel show-arrows="hover">
+    <v-carousel-item src="@/assets/img/slide1.jpg" cover></v-carousel-item>
+    <v-carousel-item src="@/assets/img/slide2.jpg" cover></v-carousel-item>
+    <v-carousel-item src="@/assets/img/slide3.jpg" cover></v-carousel-item>
+    <v-carousel-item src="@/assets/img/slide4.jpg" cover></v-carousel-item>
+  </v-carousel>
+</template>
+
+<script>
+export default {
+  name: "Carousel",
+};
+</script>
